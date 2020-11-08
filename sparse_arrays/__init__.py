c38f385468e4df21e097ea4aa3129bd5fcdcb3a7
Ali BELGHITI <belghiti.ali@gmail.com>
2020-11-08 16:25:32 +0100

    Added example in __init__.py

--- sparse_arrays/__init__.py
+++ sparse_arrays/__init__.py
@@ -13,3 +13,11 @@ def matchingStrings(strings, queries):
                 nbQueryOccurences+=1
         results[i] = nbQueryOccurences
     return results
+
+if __name__ == "__main__":
+    strings = ["def","de","fgh"]
+    queries = ["de","lmn","fgh"]
+    print(strings)
+    results = matchingStrings(strings, queries)
+    print("Result of matchingStrings when strings = " + str(strings)
+          + " and queries = " + str(queries) + ": " + str(results))
