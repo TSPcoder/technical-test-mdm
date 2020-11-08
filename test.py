5a930b4f59db2fe34e5ea02d8421eb46fc0f9469
Ali BELGHITI <belghiti.ali@gmail.com>
2020-11-08 21:01:48 +0100

    Adapted tests to make them pass

--- test.py
+++ test.py
@@ -2,22 +2,30 @@
 
 import unittest
 
-from sparse_arrays import SparseArray
+from util import SparseArray as sp
+
 
 class TestSparseArrays(unittest.TestCase):
 
     def test_match_queries_with_strings(self):
-        strings=["ab","ab","abc"]
-        queries=["ab","abc","bc"]
-        results = SparseArray.matchingStrings(strings,queries)
-        expected = [2,1,0]
+        strings = ["ab", "ab", "abc"]
+        queries = ["ab", "abc", "bc"]
+        sparse_array = sp.SparseArray(queries, strings)
+
+        results = sparse_array.matchingStrings()
+        expected = [2, 1, 0]
+
         self.assertEqual(expected, results)
 
     def test_match_queries_with_strings_diff_size(self):
-        strings=["abcde","sdaklfj","asdjf","na","basdn","sdaklfj","asdjf","na","asdjf","na","basdn","sdaklfj","asdjf"]
-        queries=["abcde","sdaklfj","asdjf","na","basdn"]
-        results = SparseArray.matchingStrings(strings,queries)
-        expected = [1,3,4,3,2]
+        strings = ["abcde", "sdaklfj", "asdjf", "na", "basdn", "sdaklfj", "asdjf", "na", "asdjf", "na", "basdn",
+                   "sdaklfj", "asdjf"]
+        queries = ["abcde", "sdaklfj", "asdjf", "na", "basdn"]
+        sparse_array = sp.SparseArray(queries, strings)
+
+        results = sparse_array.matchingStrings()
+
+        expected = [1, 3, 4, 3, 2]
         self.assertEqual(expected, results)
 
 
