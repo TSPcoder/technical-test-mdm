424adb0d8575b83450e597436fd67e02f101907a
Ali BELGHITI <belghiti.ali@gmail.com>
2020-11-08 16:00:43 +0100

    Added test and implemented function to pass the tests

--- sparse_arrays/__init__.py
+++ sparse_arrays/__init__.py
@@ -1,4 +1,15 @@
 #!/bin/python3
 
 def matchingStrings(strings, queries):
-    return strings
+    q = len(queries)
+    n = len(strings)
+    results = [0] * q
+    for i in range (0, q):
+        query = queries[i]
+        nbQueryOccurences = 0
+        for j in range (0, n):
+            string = strings[j]
+            if query == string:
+                nbQueryOccurences+=1
+        results[i] = nbQueryOccurences
+    return results
--- test.py
+++ test.py
@@ -13,6 +13,13 @@ class TestSparseArrays(unittest.TestCase):
         expected = [2,1,0]
         self.assertEqual(expected, results)
 
+    def test_match_queries_with_strings_diff_size(self):
+        strings=["abcde","sdaklfj","asdjf","na","basdn","sdaklfj","asdjf","na","asdjf","na","basdn","sdaklfj","asdjf"]
+        queries=["abcde","sdaklfj","asdjf","na","basdn"]
+        results = matchingStrings(strings,queries)
+        expected = [1,3,4,3,2]
+        self.assertEqual(expected, results)
+
 
 if __name__ == '__main__':
     unittest.main()
