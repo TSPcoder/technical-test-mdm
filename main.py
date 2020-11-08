9791008b37ff510b2be0e515c89e0315d3304ea3
Ali BELGHITI <belghiti.ali@gmail.com>
2020-11-08 20:57:44 +0100

    Refactored and modified code to take queries as parameters

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 #!/bin/python
 
-from sparse_arrays import SparseArray
+from util import SparseArray as sp
 
+import sys
+import os
 
 def main():
-    strings = ["def", "de", "fgh"]
-    queries = ["de", "lmn", "fgh"]
+    #opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 
-    results = SparseArray.matchingStrings(strings, queries)
+    # Determining the 'queries' array from the input arguments
+    queries = []
+    nb_queries = 0
+    for query in args:
+        queries.append(query)
+        nb_queries+=1
 
-    print("Result of matchingStrings when strings = " + str(strings)
-          + " and queries = " + str(queries) + ": " + str(results))
+    #strings = os.getenv('STRINGS_ARRAY')
+    strings = ['abc','a','bd']
+    sparse_array = sp.SparseArray(queries, strings)
+
+    results = sparse_array.matchingStrings()
+
+    resultsDict = {}
+    for i in range (0, nb_queries):
+        resultsDict[queries[i]] = results[i]
+
+    print("Number of occurences for which queries array " + str(queries) + " appear in the 'strings' array "
+          + str(strings) + " : " + "\n" + str(resultsDict))
 
 if __name__ == "__main__":
     main()
---
+++ util/SparseArray.py
@@ -0,0 +1,31 @@
+#!/bin/python
+
+class SparseArray:
+    queries: list
+    strings: list
+    """Class defined by:
+    - a collection of input strings named 'strings'
+    - a collection of query strings name 'query' """
+
+    def __init__(self, queries, strings):
+        self.queries = queries
+        self.strings = strings
+
+
+    def matchingStrings(self):
+        q = len(self.queries)
+        n = len(self.strings)
+        results = [0] * q
+
+        for i in range(0, q):
+            query = self.queries[i]
+            nb_query_occurrence = 0
+
+            for j in range(0, n):
+                string = self.strings[j]
+                if query == string:
+                    nb_query_occurrence += 1
+
+            results[i] = nb_query_occurrence
+
+        return results
