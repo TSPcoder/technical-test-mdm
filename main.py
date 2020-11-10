fb1d4239923cf25c1f4bfe46b8c5cea8767e723c
Ali BELGHITI <belghiti.ali@gmail.com>
2020-11-10 15:43:51 +0100

    Added Swagger UI to use Flask API more easily

--- main.py
+++ main.py
@@ -5,17 +5,28 @@ from util import RandomArrayGenerator as rand
 
 import os
 from flask import Flask, jsonify
-
 from flask_restplus import Resource, Api
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 api = Api(app, title='SparseArrays', description='Matching queries with strings')
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "SparseArrays Flask API"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 
 @api.route('/query/<string:query>')
 class MatchingStrings(Resource):
 
-    def get(self, query=''):
+    def get(self, query):
         list_results = main(query)
         results_query_occurences = list_results[0]
         strings_array = list_results[1]
@@ -29,17 +40,14 @@ def main(query):
     nb_queries = len(queries)
 
     if os.getenv('STRINGS_ARRAY') is None:
-
         # Generating 'strings' array from the below input arguments
         min_range = 2
         max_range = 6
         max_length = 2
-
         random_array_generator = rand.RandomArrayGenerator(min_range, max_range, max_length)
         strings = random_array_generator.getRandomArrayGenerator()
-
     else:
-        strings = os.getenv('STRINGS_ARRAY').split(",")
+        strings = os.environ['STRINGS_ARRAY'].split(",")
 
     sparse_array = sp.SparseArray(queries, strings)
     results = sparse_array.matchingStrings()
@@ -50,7 +58,7 @@ def main(query):
         results_dict[queries[i]] = results[i]
 
     # Saving the result in a list of 2 elements
-    # - number of query occurences
+    # - number of query occurrences
     # - 'strings' array
     return results_dict, strings
 
